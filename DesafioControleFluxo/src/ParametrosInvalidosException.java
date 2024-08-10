0a3bd0f21391e655fda9509d33fd7381f144c88f
Vinícius Diefenbach <viniciusdiefenbach2004@gmail.com>
2024-08-10 17:24:18 -0300

    Criar v1.0 da solução do desafio dio java controle de fluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+
+public class ParametrosInvalidosException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException() {
+		super("O segundo parâmetro deve ser maior que o primeiro");
+	}
+}
